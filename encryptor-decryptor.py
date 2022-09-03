d308267dd9ee2cc199256a5e8e1741429a2fe227
MD Tareq Shah Alam <67548796+Tareq905@users.noreply.github.com>
2022-09-04 01:18:57 +0600

    Add files via upload

---
+++ encryptor-decryptor.py
@@ -0,0 +1,92 @@
+from tkinter import *
+from tkinter import  messagebox
+import base64
+import os
+
+def decrypt():
+    password = code.get()
+    
+    if password == "4321":
+        screen2 = Toplevel(screen)
+        screen2.title("decryption")
+        screen2.geometry("400x200")
+        screen2.configure(bg="#FDDCDC")
+        
+        message = text1.get(1.0,END)
+        decode_message = message.encode("ascii")
+        base64_bytes = base64.b64decode(decode_message)
+        decrypt=base64_bytes.decode("ascii")
+        
+        Label(screen2,text="DECRYPT",font="arial", fg="white",bg="#00bd56").place(x=10,y=0)
+        text2 = Text(screen2, font="Rpbote 10", bg="white",relief=GROOVE,wrap=WORD,bd=0)
+        text2.place(x=10,y=40,width=380,height=150)
+        
+        text2.insert(END,decrypt)
+        
+    elif password == "":
+        messagebox.showerror("encryption","Input Password")
+    elif password != "4321":
+        messagebox.showerror("encryption","Input Password")
+
+def encrypt():
+    password = code.get()
+    
+    if password == "4321":
+        screen1 = Toplevel(screen)
+        screen1.title("encryption")
+        screen1.geometry("400x200")
+        screen.configure(bg="#FDDCDC")
+        
+        message = text1.get(1.0,END)
+        encode_message = message.encode("ascii")
+        base64_bytes = base64.b64encode(encode_message)
+        encrypt=base64_bytes.decode("ascii")
+        
+        Label(screen1,text="ENCRYPT",font="arial", fg="white",bg="#7DA08B").place(x=10,y=0)
+        text2 = Text(screen1, font="monospace", bg="white",relief=GROOVE,wrap=WORD,bd=0)
+        text2.place(x=10,y=40,width=380,height=150)
+        
+        text2.insert(END,encrypt)
+        
+    elif password == "":
+        messagebox.showerror("encryption","Input Password")
+    elif password != "4321":
+        messagebox.showerror("encryption","Input Password")
+    
+    
+
+def main_screen():
+    
+    global screen
+    global code
+    global text1
+    
+    screen = Tk()
+    screen.geometry("375x398")
+
+    image_icon = PhotoImage(file="logo.png")
+    screen.iconphoto(False,image_icon)
+    screen.title("Shadow")
+    
+    def reset():
+        code.set("")
+        text1.delete(1.0,END)
+    
+    Label(text="Enter text for encryption and decryption", fg="black", font=("fantacy",13)).place(x=10,y=10)
+    text1 = Text(font="monospace", bg = "white", relief=GROOVE,wrap=WORD,bd=0)
+    text1.place(x=10,y=50, width=355, height=100)
+    
+    Label(text="Enter text for encryption and decryption", fg="black", font=("fantacy",13)).place(x=10,y=170)
+    
+    code = StringVar()
+    Entry(textvariable=code,width=19,bd=0,font=("verdana",25),show="*").place(x=10,y=200)
+    
+    
+    Button(text="Encrypt", height="2",width=23,bg="#E74502", fg="white",bd=0, command=encrypt).place(x=10,y=250)
+    Button(text="Decrypt", height="2",width=23,bg="#195B44", fg="white",bd=0, command=decrypt).place(x=200,y=250)
+    Button(text="Reset", height="2",width=23,bg="#5B5719", fg="white",bd=0, command=reset).place(x=10,y=300)
+    
+    
+    screen.mainloop()
+    
+main_screen()
